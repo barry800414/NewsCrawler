0a029bf6848810781e21610ee8ce12c39c5e1c57
barry800414 <barry800414@gmail.com>
2014-12-08 18:31:33 +0800

    update json & crawler

--- news_crawler/spiders/crawler.py
+++ news_crawler/spiders/crawler.py
@@ -66,48 +66,130 @@ class NewsSpider(CrawlSpider):
         news = NewsItem()
         #print response.url
         news['url'] = response.url
+        #print response.header
+        #print response.status
 
         # try different xpath
         if self.debug:
-            print ''
+            print '=================================================='
         for field in self.config['xpath'].keys():
             news[field] = None
             for x in self.config['xpath'][field]: 
-                tmp = response.xpath(x['xpath']).extract() 
+                parsed_results = response.xpath(x['xpath']).extract() 
                 
-                # debug mode
+                # if debug mode, print out debug messages
                 if self.debug:
-                    self.print_parsed_result(field, x['xpath'], tmp)
-            
-                # to merge all matched results 
-                if x['expect'] == -1 and x['index'] == 'merge_all':
-                    news[field] = self.merge_result(tmp)
+                    self.print_parsed_results(field, x['xpath'], parsed_results)
+                # if there are not only one content (e.g. parsed_results = ['aaa', 'bbb', ...])
+                if x['expect'] == -1:
+                    # merge all of them
+                    if x['index'] == 'merge_all':
+                        news[field] = self.merge_results(parsed_results, sep=u'\n')
+                    elif type(x['index']) is list:
+                        news[field] = self.merge_some_of_results(parsed_results, x['index'], sep=u'\n')
+                    elif type(x['index']) is int:
+                        news[field] = self.get_one_of_results(parsed_results, x['index'])
+                    elif type(x['index']) is str or type(x['index']) is unicode:
+                        news[field] = self.merge_some_of_results_by_index_str(parsed_results, x['index'], sep='\n')
+                  
                     if news[field] != None:
                         break
-
-                # extract only one result
+                
+                # if there should be certain contents 
+                # (e.g. x['expect'] = 3, parsed_results = ['a', 'b', 'c'])
                 else:
-                    if len(tmp) == x['expect']: 
-                        if len(tmp[x['index']].strip()) != 0:
-                            news[field] = tmp[x['index']].strip()
-                            break
+                    if len(parsed_results) == x['expect']: 
+                        news[field] = self.get_one_of_results(parsed_results, x['index'])
+                        break
+
         return news  #then the object will fed into pipeline
 
-    def merge_result(self, parsed_result):
-        if len(parsed_result) == 0:
+    # merge all of them into one string 
+    def merge_results(self, parsed_results, sep=''):
+        if len(parsed_results) == 0:
             return None
         merge = u''
-        for s in parsed_result:
-            merge += s.strip() + u'\n'           
-        if len(merge.strip()) == 0:
+        for s in parsed_results:
+            merge += s.strip() + sep
+        merge = merge.strip()
+
+        if len(merge) == 0:
             return None
         else:
-            return merge.strip()
+            return merge
 
-    def print_parsed_result(self, field, xpath, parsed_result):
-        print field, '     ', xpath, '     ', parsed_result
-        for s in parsed_result:
+    # merge some of them into one string
+    def merge_some_of_results(self, parsed_results, index_list, sep=''):
+        if parsed_results == None:
+            return None
+
+        merge = u''
+        for index in index_list:
+            r = self.get_one_of_results(parsed_results, index)
+            if r != None:
+                merge += r + sep
+        merge = merge.strip()
+        if len(merge) == 0:
+            return None
+        else:
+            return merge
+    
+    # index_str: "2:-1" means 3rd element to (last-1)-th element 
+    def merge_some_of_results_by_index_str(self, parsed_results, index_str, sep=''):
+        if len(parsed_results) == 0:
+            return None
+        indexes = index_str.split(':')
+        start = None
+        end = None
+        assert len(indexes) == 2
+        if len(indexes[0].strip()) != 0:
+            start = int(indexes[0].strip())
+        if len(indexes[1].strip()) != 0:
+            end = int(indexes[1].strip())
+    
+        if start == None:
+            if end == None:
+                pass
+            else:
+                parsed_results = parsed_results[:end]
+        else:
+            if end == None:
+                parsed_results = parsed_results[start:]
+            else:
+                parsed_results = parsed_results[start:end]
+        
+
+        return self.merge_results(parsed_results, sep = u'\n')
+    
+
+    # get one of them
+    def get_one_of_results(self, parsed_results, index):
+        if parsed_results == None:
+            return None
+
+        num = len(parsed_results)
+        if self.is_valid_index(num, index):
+            r = parsed_results[index].strip()
+            if len(r) == 0:
+                return None
+            else:
+                return r
+        else:
+            return None
+    
+    def is_valid_index(self,arr_length, index):
+        if (index >= 0 and index < arr_length) or (index < 0 and index >= (-1*arr_length)):
+            return True
+        else:
+            return False
+
+
+    def print_parsed_results(self, field, xpath, parsed_results):
+        print field, '     ', xpath, '     ', parsed_results
+        for s in parsed_results:
             print s.encode('utf-8')
+
+
 
 
 # for disabling dropping messages
